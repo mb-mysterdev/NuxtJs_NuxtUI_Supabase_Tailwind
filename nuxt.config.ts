85c8fa45788e7e342ff28e09add267cd7571d169
mysterdev <mohamed.bouhlel.dev@gmail.com>
2024-04-07 21:13:19 +0200

    wip

---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  extends: [process.env.NUXT_UI_PRO_PATH || '@nuxt/ui-pro'],
+  modules: [
+    '@nuxt/ui',
+    '@nuxt/fonts',
+    '@vueuse/nuxt',
+    '@nuxtjs/supabase'
+  ],
+  supabase: {
+    redirect: false
+  },
+  ui: {
+    icons: ['heroicons', 'simple-icons'],
+    safelistColors: ['primary', 'red', 'orange', 'green']
+  },
+  devtools: {
+    enabled: true
+  }
+})
